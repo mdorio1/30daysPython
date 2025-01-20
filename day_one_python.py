2ab8339cae206de275c020f5cbbb8d162e41fa8c
mdorio1 <datavisualsmad@gmail.com>
2025-01-20 09:36:54 -0600

    testing Python
    created a test python script to import, will be uploading more for testing and learning

---
+++ day_one_python.py
@@ -0,0 +1,13 @@
+print("day 1 Python Learning")
+
+
+
+print("Var case is important!")
+first_name = "Mike"
+FIRSTNAME = "Michael"
+FirstName = "Mikey"
+
+print(FIRSTNAME) #this exercise shows the importance of case senstitive, when typing var case is required
+#print(First_Name) # becauase the var is first_name not First_Name python throws an error that the var is not def
+print(first_name) # when used correctly python will understand what var you are using
+
